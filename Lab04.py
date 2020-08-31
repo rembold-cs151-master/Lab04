465154878657c53cac92b7a41661e53eceea6ec1
Jed Rembold <jedediah.r@gmail.com>
2020-08-31 12:02:47 -0700

    Change to 99 bottles loop lab

---
+++ Lab04.py
@@ -0,0 +1,57 @@
+# File: Lab04.py
+
+"""
+This lab is to focus and practice writing loops of both varieties.
+"""
+
+
+def bottle_song_while(num):
+    """
+    Insert your code to print out the lines to the 99 bottles song
+    in the space below!  You should use a WHILE loop in this function 
+    to achieve this. The num argument to this function is the starting
+    point for the number of bottles.
+
+    Remember that each "verse" should take 4 lines, and then
+    a blank link should be printed between verses. 
+
+    You can delete the below "pass" once you add some code. It is just
+    here to stop syntax errors until you've added actual code to
+    this function.
+    """
+    pass
+
+
+
+
+
+
+
+
+
+def bottle_song_for(num):
+    """
+    Insert your code to print out the lines to the 99 bottles song
+    in the space below!  You should use a FOR loop in this function 
+    to achieve this. The num argument to this function is the starting
+    point for the number of bottles. Using range() will be useful!
+
+    Remember that each "verse" should take 4 lines, and then
+    a blank link should be printed between verses. 
+
+    You can delete the below "pass" once you add some code. It is just
+    here to stop syntax errors until you've added actual code to
+    this function.
+    """
+    pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    # Uncomment or comment the below lines depending on which
+    # function you want to be run!
+    bottle_song_while(10)
+    bottle_song_for(10)
--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# Lab 04 - Troubleshooting
-![](https://github.com/<user_info>/workflows/Autotesting/badge.svg)
+# Lab 04 - Loops!
 
-You are tasked with fixing the two scripts included in this repository.
+The goal for this lab is to print out the lyrics to the "99 bottles of beer on the wall" song using both a while loop method and a for loop method. More details in the instructions.
 
-Instructions for what both scripts are doing are included in the overall lab instructions.
-
-Fix both locally on your own system after downloading the repository and then upload them back to Github to get credit for the day.
-
-Autotesting is running on these, so you will be able to check them just like with your homework!
+No autotesting on this lab, since it is a bit open-ended and flexible in its presentation. Just make sure both functions are printing out the same lyrics!
